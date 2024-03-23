b074b7f13c9286cafe34db442503eda57adcf3e6
edalsare <easrevelo@gmail.com>
2024-03-23 15:34:55 -0500

    primer commit

---
+++ src/main/java/com/ibero/logisticEAS/LogisticEasApplication.java
@@ -0,0 +1,13 @@
+package com.ibero.logisticEAS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogisticEasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogisticEasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ibero/logisticEAS/LogisticEasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibero.logisticEAS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogisticEasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
